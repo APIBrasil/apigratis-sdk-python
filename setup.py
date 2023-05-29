ccd9aa4f5b63d6e40ad0b8f80a9df6d9d5bc2cee
jhowbhz <jhowjhoe@icloud.com>
2023-05-29 18:11:52 -0300

    two test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='apigratis-sdk-python',
-    version='0.0.1',
+    version='0.0.3',
     description='Transforme seus projetos em soluções inteligentes com nossa API. Com recursos como  API do WhatsApp, geolocalização, rastreamento de encomendas, verificação de CPF/CNPJ e mais, você pode criar soluções eficientes e funcionais. Comece agora.',
     author='APIBRASIL',
     author_email='contato@apibrasil.com.br',
