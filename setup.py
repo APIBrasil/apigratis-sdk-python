d96adab601c4aa0f1c0f6930f6df2bb4cde7535b
jhowbhz <jhowjhoe@icloud.com>
2023-05-30 09:55:08 -0300

    big fix 0.0.1-stable

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
-setup(
-    name='apigratis',
-    version='0.1.0',
-    description='Transforme seus projetos em soluções inteligentes com nossa API. Com recursos como  API do WhatsApp, geolocalização, rastreamento de encomendas, verificação de CPF/CNPJ e mais, você pode criar soluções eficientes e funcionais. Comece agora.',
-    author='APIBRASIL',
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='apigratis',
+    version='0.0.1',
+    url='https://github.com/caiocarneloz/apigratis',
+    license='MIT License',
+    author='Caio Carneloz',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author_email='contato@apibrasil.com.br',
+    keywords='whatsapp api, apibrasil, cnpj, sms, cep, consulta, api, brasil, gratis, free, whatsapp, apiwhatsapp, apigratis, apifree',
+    description=u'Transforme seus projetos em soluções inteligentes com nossa API. Com recursos como API do WhatsApp, geolocalização, rastreamento de encomendas, verificação de CPF/CNPJ e mais, você pode criar soluções eficientes e funcionais',
     packages=['apigratis'],
-    install_requires=[
-        # Lista de dependências necessárias
-        'requests',
-    ],
-)
+    install_requires=['numpy'],)
