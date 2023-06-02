90d7c46c8365d02fd49b42e320e26747571245f0
jhowbhz <jhowjhoe@icloud.com>
2023-06-02 14:03:32 -0300

    fix v1.0.4

--- build/lib/apigratis-sdk-python/Service.py
+++ build/lib/apigratis-sdk-python/Service.py
@@ -3,35 +3,21 @@ import json
 
 class Service:
 
-    def whatsapp(dados):
+    server = None
 
+    def __init__(self):
+        self.server = "https://cluster-01.apigratis.com/api/v1/"
+
+    def request(self, service, dados):
         try:
-            server = "https://cluster-01.apigratis.com/api/v1/whatsapp/"
-
+            
+            action = data.get('action', '')
             data = json.loads(dados)
 
-            if not data['action']:
-                raise Exception('Action não informada, verifique a documentação')
-
-            action = data['action']
-
-            url = server+str(action)
-
-            if not data['body']:
-                raise Exception('Body não informado, verifique a documentação')
-
+            credentials = data['credentials']
             payload = json.dumps(data['body'])
 
-            credentials = data['credentials']
-
-            if not credentials['SecretKey']:
-                raise Exception('SecretKey não informado')
-            if not credentials['PublicToken']:
-                raise Exception('PublicToken não informado')
-            if not credentials['BearerToken']:
-                raise Exception('BearerToken não informado')
-            if not credentials['DeviceToken']:
-                raise Exception('DeviceToken não informado')
+            url = self.server + str(service) + str(action)
 
             headers = {
                 'Content-Type': 'application/json',
@@ -41,13 +27,30 @@ class Service:
                 'DeviceToken': credentials['DeviceToken']
             }
 
-            agent = 'APIBRASIL\Whatsapp/1.0.0' 
-
+            agent = 'APIBRASIL/PYTHON-SDK'
             headers['User-Agent'] = agent
 
             response = requests.request("POST", url, headers=headers, data=payload, allow_redirects=False, stream=True, proxies=None)
 
-            return json.loads(response.text.encode('utf8'))
+            return json.loads(response.text.encode('utf8')) 
 
         except Exception as e:
             return {'error': str(e)}
+
+    def whatsapp(self, dados):
+        return self.request('whatsapp', dados)
+
+    def vehicles(self, dados):
+        return self.request('vehicles', dados)
+
+    def correios(self, dados):
+        return self.request('correios', dados)
+
+    def cep(self, dados):
+        return self.request('cep', dados)
+
+    def cnpj(self, dados):
+        return self.request('dados/cnpj', dados)
+
+    def cpf(self, dados):
+        return self.request('dados/cpf', dados)
